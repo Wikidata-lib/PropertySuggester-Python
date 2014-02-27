50b3e134b5d229d07aab037d57f2dc7ca077c613
Christian Dullweber <xchrdw@gmail.com>
2014-02-27 15:18:31 +0100

    fix path

--- propertysuggester/test/test_xml_reader.py
+++ propertysuggester/test/test_xml_reader.py
@@ -18,8 +18,8 @@ class XmlReaderTest(AbstractUniverseTest):
 
 class MultiprocessingBigTest(TestCase):
     def test_simple_multiprocessing(self):
-        r1 = list(XmlReader.read_xml(gzip.open("Wikidata-Q1.xml.gz"), 1))
-        r4 = list(XmlReader.read_xml(gzip.open("Wikidata-Q1.xml.gz"), 4))
+        r1 = list(XmlReader.read_xml(gzip.open(resource_filename("propertysuggester.test", "Wikidata-Q1.xml.gz")), 1))
+        r4 = list(XmlReader.read_xml(gzip.open(resource_filename("propertysuggester.test", "Wikidata-Q1.xml.gz")), 4))
 
         self.assertThat(r1, HasLength(1))
         self.assertThat(r4, Equals(r1))
