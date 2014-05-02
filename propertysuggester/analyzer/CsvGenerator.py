6e4f56ffd7c70e66138da6ea1d5ba585d1f04b60
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-05-02 13:01:41 +0200

    define lineterminator in csvGenerator.py

--- propertysuggester/analyzer/CsvGenerator.py
+++ propertysuggester/analyzer/CsvGenerator.py
@@ -7,7 +7,7 @@ def create_pair_csv(table, out, delimiter=","):
     @type out: file or StringIO.StringIO
     @type delimiter: string
     """
-    csv_writer = csv.writer(out, delimiter=delimiter, quoting=csv.QUOTE_MINIMAL)
+    csv_writer = csv.writer(out, delimiter=delimiter, quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
     print "properties: {0}".format(len(table))
 
     csv_writer.writerow(("pid1", "qid1", "pid2", "count", "probability", "context"))
@@ -16,7 +16,7 @@ def create_pair_csv(table, out, delimiter=","):
         for pid2, value in row.iteritems():
             if pid1 != pid2 and isinstance(pid2, int) and value > 0:  # "appearances" is in the same table, ignore them
                 probability = value/float(row["appearances"])
-                csv_writer.writerow((pid1, '', pid2, value, probability, "item"))
+                csv_writer.writerow((pid1, '', pid2, value, probability, 'item'))
                 rowcount += 1
                 if not rowcount % 1000:
                     print "rows {0}".format(rowcount)
