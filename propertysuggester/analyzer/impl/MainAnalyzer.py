2edaa09a9d8541c655ec985a035af9ab03f0bb46
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-07-06 21:51:02 +0200

    consider classifying properties in analysis

--- propertysuggester/analyzer/impl/MainAnalyzer.py
+++ propertysuggester/analyzer/impl/MainAnalyzer.py
@@ -2,28 +2,40 @@ from collections import defaultdict
 from propertysuggester.analyzer.impl.Analyzer import Analyzer
 from propertysuggester.analyzer.rule import Rule
 
+classiying_property_ids = [31,279]
 
 class ItemAnalyzer(Analyzer):
     def __init__(self):
         Analyzer.__init__(self)
-        self.property_occurances = defaultdict(int)
-        self.pair_occurances = defaultdict(lambda: defaultdict(int))
+        self.tuple_occurrences = defaultdict(int)
+        self.pair_occurrences = defaultdict(lambda: defaultdict(int))
     
-    def process(self, entity):
+    def process(self, item):
         distinct_ids = set(claim.mainsnak.property_id for claim in entity.claims)
-        self._count_occurances(distinct_ids)
+        property_value_pairs = claim.mainsnak.property_id, claim.mainsnak.value for claim in entity.claims
+        self._count_occurrences(distinct_ids, property_value_pairs)
 
-    def _count_occurances(self, distinct_ids):
+    def _count_occurrences(self, distinct_ids, property_value_pairs):
         for pid1 in distinct_ids:
-            self.property_occurances[pid1] += 1
+            if pid1 in classiying_property_ids:
+                continue
+            currentTuple = (pid1, None)
+            self.tuple_occurrences[currentTuple] += 1
             for pid2 in distinct_ids:
                 if pid1 != pid2:
-                    self.pair_occurances[pid1][pid2] += 1
+                    self.pair_occurrences[currentTuple][pid2] += 1
+
+        for pid1, value in property_value_pairs:
+            if pid in classiying_property_ids:
+                self.tuple_occurrences[pid, value] += 1
+                for pid2 in distinct_ids:
+                    if pid1 != pid2:
+                        self.pair_occurrences[pid1, value][pid2] += 1
 
     def get_rules(self):
         rules = []
-        for pid1, row in self.pair_occurances.iteritems():
-            pid1count = self.property_occurances[pid1]
+        for pid1, row in self.pair_occurrences.iteritems():
+            pid1count = self.property_occurrences[pid1]
             for pid2, paircount in row.iteritems():
                 if paircount > 0:
                     probability = (paircount/float(pid1count))
